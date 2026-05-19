ce1f1b211945423de20eebcae9ef7ba1f9d1a0ca
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2026-05-19 11:12:48 +0800

    feat: deployment to cloudflare and vercel prep

--- app/projects/[id]/page.js
+++ app/projects/[id]/page.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import api from "@/lib/api";
 
+// export const runtime = "edge";
+
 async function getProject(id) {
   try {
     const res = await api.get(`/projects/${id}`);
